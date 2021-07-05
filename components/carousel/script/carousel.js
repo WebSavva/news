66deedd58e1ea497fa22668604618ae7d079bda1
WebSavva <70653174+WebSavva@users.noreply.github.com>
2021-07-05 14:43:12 +0300

    Carousel modification: two types are added - small and large, dot controls also have been added

--- components/carousel/script/carousel.js
+++ components/carousel/script/carousel.js
@@ -1,110 +1,188 @@
-// document.addEventListener('DOMContentLoad', () => {});
-
 class Carousel {
-    constructor(config) {
-        //initializaing the configuration
-        this._config = config;
-        this.slideDuration = this._config.interval;
-        this.id = this._config.id;
-        this.breakpoints = this._config.breakpoints;
-        this.currentDirection = "right";
+  constructor({type = "small", breakpoints = {}, interval = 3e3, id, dotControlsColor = "gray", dotControlsPosition = "top-right" } = {}) {
+    //initializaing the configuration 
+    this.type = type
+    
+    this.id = id;
+    this.slideDuration = interval;
+    this.breakpoints = breakpoints;
+    this.currentDirection = "right";
+    this.carouselStart = 0;
+    this.currentSlide = this.carouselStart;
+    this.dotControlsColor = dotControlsColor;
+    this.dotControlsPosition = dotControlsPosition;
 
-        this.carouselElement = document.getElementById(this.id);
-        this.items = Array.from( this.carouselElement.querySelectorAll('.carousel__item') );
-        this.itemsNumber = this.items.length;
-        this.carouselStart = 0;
-        this.controls = Array.from( document.body.querySelectorAll(`[data-carousel-target="${this.id}"`) );
-        this.carouselContent = this.carouselElement.querySelector('.carousel__content');
-        this.setUpCarousel();
-    }
+    this.carouselElement = document.getElementById(this.id);
+    this.items = Array.from(
+      this.carouselElement.querySelectorAll(".carousel__item")
+    );
+    this.itemsNumber = this.items.length;
+    this.carouselContent = this.carouselElement.querySelector(".carousel__content");
+    this.controls = Array.from( document.body.querySelectorAll(`[data-carousel-target="${this.id}"`) );
+    this.setUpCarousel();
+  }
 
-    setUpCarousel() {
-        this.calculateSize();
+  setUpCarousel() {
+    this.calculateSize();
 
-        this.currentSlide = this.carouselStart;
-
-        this.controls.forEach((control) => control.addEventListener('click', ({target}) => {
-
-            clearTimeout(this.delayedAutoMove);
-            clearInterval(this.carouselInterval);
-
-            const currentControl = target.closest('[data-carousel-target]');
-
-            if (!currentControl) return;
-
-            const moveDirection = currentControl.dataset.carouselDirection;
-
-            this.moveContent(moveDirection);
-
-            this.delayedAutoMove = setTimeout(() => {
-                this.setAutoMove();
-            }, this.slideDuration * 2);
-        }));
-
+    if ( this.type === "large" ) {
         window.onresize = () => {
-            console.log('fired');
             this.calculateSize();
             this.moveContent("right");
         };
-
-        this.setAutoMove();
+    } else if ( this.type === "small" ) {
+        this.setUpDotControls();
     }
 
-    moveContent(direction = this.currentDirection) {
-        
-        let fastMove = false;
-        if (direction === "left") {
-            if (this.currentSlide === this.carouselStart ) {
-                this.currentSlide = this.carouselEnd;
-                fastMove = true;
-            } else {
-                this.currentSlide--
-            }    
-        } else {
-            if (this.currentSlide === this.carouselEnd) {
-                this.currentSlide = this.carouselStart;
-                fastMove = true;
-            } else {
-                this.currentSlide++;
-            }
-        }
 
-        if (fastMove) {
-            this.carouselContent.style.transition = "all 1s";
-        } else {
-            this.carouselContent.style.transition = "";
-        }
+    this.controls.forEach((control) =>
+      control.addEventListener("click", ({ target }) => {
+          this.controlClickHandle(target);
+      })
+    );
 
-        this.carouselContent.style.transform = `translateX(-${ this.currentSlide  * (100 / this.itemsPerSlide)}%)`;
+
+    this.setAutoMove();
+  }
+
+  moveContent(direction = this.currentDirection) {
+    let fastMove = false;
+    if (direction === "left") {
+      if (this.currentSlide === this.carouselStart) {
+        this.currentSlide = this.carouselEnd;
+        fastMove = true;
+      } else {
+        this.currentSlide--;
+      }
+    } else {
+      if (this.currentSlide === this.carouselEnd) {
+        this.currentSlide = this.carouselStart;
+        fastMove = true;
+      } else {
+        this.currentSlide++;
+      }
     }
 
-    setAutoMove() {
-        this.carouselInterval = setInterval(() => {
-            if (this.currentSlide === this.carouselStart ) this.currentDirection = "right";
-            if (this.currentSlide === this.carouselEnd ) this.currentDirection = "left";
-            this.moveContent();
-        }, this.slideDuration);
+    if (fastMove) {
+      this.carouselContent.style.transition = "all 1s";
+    } else {
+      this.carouselContent.style.transition = "";
     }
 
-    calculateSize() {
-        let currentBreakpoint;
-        for (let breakpoint of Object.keys(this.breakpoints) ) {
-            console.log(window.innerWidth);
-            if (+breakpoint <= window.innerWidth) {
-               currentBreakpoint = breakpoint;
-            }
-        }
+    this.carouselContent.style.transform = `translateX(-${
+      this.currentSlide * (100 / this.itemsPerSlide)
+    }%)`;
 
-        if (!currentBreakpoint) {
-            currentBreakpoint = Object.keys(this.breakpoints).reduce((prev, next) => {
-                return +prev < +next ? +next : +prev;
-            });
-        }
+    if (this.type === "small") {
+        this.dotElements.forEach((dot) => dot.classList.remove('carousel__dot--selected'));
+        this.dotElements[this.currentSlide].classList.add('carousel__dot--selected');
+    }
+  }
 
+  controlClickHandle(target) {
+      
+    const currentControl = target.closest("[data-carousel-target]");
+      
+    if (!currentControl) return;
+      
+    clearTimeout(this.delayedAutoMove);
+    clearInterval(this.carouselInterval);
+    const moveDirection = currentControl.dataset.carouselDirection;
+
+    this.moveContent(moveDirection);
+
+    this.delayedAutoMove = setTimeout(() => {
+      this.setAutoMove();
+    }, this.slideDuration * 2);
+  }
+
+  setAutoMove() {
+    this.carouselInterval = setInterval(() => {
+      if (this.currentSlide === this.carouselStart)
+        this.currentDirection = "right";
+      if (this.currentSlide === this.carouselEnd)
+        this.currentDirection = "left";
+      this.moveContent();
+    }, this.slideDuration);
+  }
+
+  calculateSize() {
+    
+    let currentBreakpoint;
+    for (let breakpoint of Object.keys(this.breakpoints)) {
+      if (+breakpoint <= window.innerWidth) {
+        currentBreakpoint = breakpoint;
+      }
+    }
+
+    if (this.type === "large") {
         this.itemsPerSlide = this.breakpoints[currentBreakpoint];
         this.carouselEnd = this.itemsNumber - this.itemsPerSlide;
-        this.currentSlide = 0;
-        
-        this.items.forEach( (item) => item.style.flex = `0 0 ${100 / (this.itemsPerSlide ?? 1) }%`);
+    } else if (this.type === "small") {
+        this.itemsPerSlide = 1;
+        this.carouselEnd = this.itemsNumber - 1;
     }
+
+    this.items.forEach(
+      (item) => (item.style.flex = `0 0 ${100 / this.itemsPerSlide}%`)
+    );
+  }
+
+  setUpDotControls() {
+    //creating elements
+    this.dotsWrapper = document.createElement('div');
+    this.dotsWrapper.className = "carousel__dots-wrapper";
+    
+    this.dotElements = [];
+    for (let i = 0; i < this.itemsNumber; i++) {
+        const dot = document.createElement('span');
+        dot.className = "carousel__dot";
+        dot.setAttribute('data-carousel-slide', i);
+        this.dotElements.push(dot);
+    }
+    this.dotsWrapper.append(...this.dotElements);
+    
+    this.dotsWrapper.style.cssText = `--carousel-dot-color:${this.dotControlsColor}`;
+    switch (this.dotControlsPosition) {
+        case "bottom":
+            this.dotsWrapper.style.bottom = "10%";
+            this.dotsWrapper.style.left = "50%";
+            this.dotsWrapper.style.transform = "translateX(-50%)";
+            break;
+        case "top-right":
+        default:
+            this.dotsWrapper.style.top = "10%";
+            this.dotsWrapper.style.right = "10%";
+            break;
+    }
+    
+
+    //attaching event listener
+    this.dotsWrapper.addEventListener('click', ({target}) => {
+        if (! target.classList.contains('carousel__dot') ) return;
+
+        this.currentSlide = +target.dataset.carouselSlide;
+        
+        this.carouselContent.style.transform = `translateX(-${this.currentSlide * 100}%)`;
+    });
+
+    this.carouselElement.append(this.dotsWrapper);
+
+  }
 }
+
+window.onload = () => {
+  const carousel1 = new Carousel({
+    id: "carousel1",
+    breakpoints: {
+        900: 3,
+        700: 2,
+        500: 1
+    },
+    interval: 3e3,
+    type: "large",
+    // dotControlsPosition: "bottom",
+    // dotControlsColor: "red"
+  });
+  console.log(carousel1);
+};
